66eaef0fc45d2e55705cf421631a398728bf637a
Pavani <vemasani.pavani@gmail.com>
2024-05-10 17:49:23 +0530

    provide permissions through policy

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "tools" {
   source = "./module/app"
   tool_name = each.key
   instance_type = each.value["instance_type"]
-  policy_request_list  = each.value["policy_request_list"]
+  policy_request_list  = each.value
   aws_ami = "ami-090252cbe067a9e58"
   zone_id = var.zone_id
 
