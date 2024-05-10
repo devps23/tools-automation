446b53ca454fe9276903c90f18a57a0b0112c200
Pavani <vemasani.pavani@gmail.com>
2024-05-10 18:26:25 +0530

    malformed sysntax error

--- module/app/variable.tf
+++ module/app/variable.tf
@@ -4,5 +4,5 @@ variable "aws_ami" {}
 variable "zone_id" {}
 variable "policy_request_list" {}
 variable "describe_instance_type"{
-  default = "DescribeInstanceTypes"
+  default = ["ec2:DescribeInstanceTypes"]
 }
