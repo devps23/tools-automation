3b2926b5e510499b0f16c65150eb9faa366b79bf
Pavani <vemasani.pavani@gmail.com>
2024-05-11 15:39:50 +0530

    Install vault package start vault service

---
+++ vault_secrets.tf
@@ -0,0 +1,12 @@
+provider "vault" {
+  address = "https://100.26.61.51:8200"
+  skip_tls_verify =true
+  token="hvs.tyEMRhd9Ms8lvUUU1ML0kYtp"
+}
+data "vault_generic_secret" "rundeck_auth" {
+  path = "expense/demo"
+}
+resource "local_file" "file" {
+  content  = data.vault_generic_secret.rundeck_auth.data_json
+  filename = "/tmp/cred"
+}
